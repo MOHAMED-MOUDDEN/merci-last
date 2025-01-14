bda39b5a21eb9166ff78905e00bcddfc4d41f09c
MOHAMED-MOUDDEN <mohammedmoudden093@gmail.com>
2025-01-14 07:35:06 +0100

    jjjjjjj

--- resources/views/user/rooms/index.blade.php
+++ resources/views/user/rooms/index.blade.php
@@ -7,7 +7,8 @@
         @foreach($rooms as $room)
         <div class="col-md-4">
             <div class="card mb-3">
-                <img src="{{ $room->images->first() ?$room->images->first()->image_path) : 'https://via.placeholder.com/300' }}" class="card-img-top" alt="{{ $room->name }}">
+                <img src="{{ $room->images->first() ? $room->images->first()->image_path : 'https://via.placeholder.com/300' }}" class="card-img-top" alt="{{ $room->name }}">
+
                 <div class="card-body">
                     <h5 class="card-title">{{ $room->name }}</h5>
                     <p class="card-text">{{ Str::limit($room->description, 100) }}</p>
