48f73436c3f6744127e14f73bf0764098fcd30dc
MOHAMED-MOUDDEN <mohammedmoudden093@gmail.com>
2025-01-17 22:06:50 +0100

    kakakkakak

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,8 +1,8 @@
 <?php
-
 return [
+    'cloud_url' => env('CLOUDINARY_URL'),
+
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-    'api_key' => env('CLOUDINARY_API_KEY'),
+    'api_key'    => env('CLOUDINARY_API_KEY'),
     'api_secret' => env('CLOUDINARY_API_SECRET'),
-    'url' => env('CLOUDINARY_URL'),  // تأكد من أنك قد قمت بتحديد الـ URL إذا كنت تستخدمه
 ];
