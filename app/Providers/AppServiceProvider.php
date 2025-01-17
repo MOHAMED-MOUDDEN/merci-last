9a8bda41b30bc1960af1603206b14ccd2c6ba499
MOHAMED-MOUDDEN <mohammedmoudden093@gmail.com>
2025-01-17 22:39:03 +0100

    last commit about pics

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): voidd
+    public function boot(): void
     {
         Paginator::useBootstrapFour();
 
